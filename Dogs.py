a50badf722f5088a4d1680f7ea75fda8a22adc4e
ilas <ayderkhanov@yandex.ru>
2025-07-31 13:47:54 +0300

    Add function get_img

--- Dogs.py
+++ Dogs.py
@@ -4,9 +4,19 @@ import requests
 from PIL import ImageTk, Image
 from io import BytesIO
 
+URL = "https://dog.ceo/api/breeds/image/random"
 
 
+def get_img(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        return data('message')
 
+    except Exception as e:
+        messagebox.showerror("Ошибка!", f"Ошибка при запросе к API {e}")
+        return None
 
 
 def show_img():
@@ -18,11 +28,12 @@ def show_img():
             img_data=BytesIO(response.content)
             img = Image.open(img_data)
             img.thumbnail((300, 300))
+            img = ImageTk.PhotoImage(img)
             label.config(image=img)
             label.image = img
 
         except Exception as e:
-            messagebox.showerror("Ошибка!", e)
+            messagebox.showerror("Ошибка!", f"Ошибка загрузки картинки {e}")
 
 
 
